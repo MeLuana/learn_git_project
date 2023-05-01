0cf0bb22f9dc13a38c0cb0743aa329fbbc874a4e
Dinara Mozzhukhina <dinara.mozzhukhina@yandex.ru>
2023-05-01 23:01:01 +0300

    Добавлена строка "Hello world!"

--- src/main/java/com/stepup/learn_git_project/MainApplication.java
+++ src/main/java/com/stepup/learn_git_project/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.learn_git_project;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
